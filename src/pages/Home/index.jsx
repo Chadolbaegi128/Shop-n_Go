2abf2877bb8874fbff85e316bcb8807aa7bc6815
Yeonjun Jung <duswns12893@gmail.com>
2023-03-26 16:38:06 +0900

    Chore: 포트폴리오용 코드 저장(1차 프로젝트 프론트엔드)

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageSlider from './Imageslider';
+import Category from './Category';
+import Gallery from './Gallery';
+
+const Home = () => {
+  return <>
+    <Category />
+    <ImageSlider />
+    <Gallery />
+  </>
+}
+
+export default Home;
